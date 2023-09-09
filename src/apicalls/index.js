327c8fbf95fd1b098b66fe4afbf06cd363f60bd9
almustanir <olajidemubarak561@gmail.com>
2023-09-09 07:56:03 +0100

    3

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://mubswallet-backend.onrender.com",
+  baseURL: "http://localhost:4000", //place your locl host url of backend
+  //http://localhost:4000
   headers: {
     Authorization: `${localStorage.getItem("token")}`,
   },
-  timeout: 15000, // Increase the timeout to 15 seconds (or adjust as needed)
 });
-
